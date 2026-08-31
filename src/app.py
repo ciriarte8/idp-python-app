17a47276745995991af164fb633bbe1f6c16a35c
Carmen Iriarte <carmeniriarte@Carmens-MacBook-Air.local>
2026-08-31 13:03:17 +0100

    Adds new info endpoint

--- src/app.py
+++ src/app.py
@@ -4,13 +4,14 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
+@app.route('/api/v1/info')
 
-def details():
+def info():
     return jsonify ({
         'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': "You are doing great, human! <3"
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes'
     })
 
 @app.route('/api/v1/healthz')
